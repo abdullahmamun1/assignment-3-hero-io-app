48fd4a41176c8a056eae5c3af7b9f5ed46462ad7
abdullahmamun1 <hello.abdullahmamun1@gmail.com>
2026-03-15 23:25:27 +0600

    apps page added

--- src/Components/Apps.jsx
+++ src/Components/Apps.jsx
@@ -1,7 +1,50 @@
 import React from "react";
+import { useLoaderData } from "react-router";
+import AppCard from "./AppCard";
 
 const Apps = () => {
-  return <div>Apps</div>;
+  const apps = useLoaderData();
+  return (
+    <div className="my-17.5 md:my-35 min-h-screen">
+      <h1 className="text-5xl font-semibold text-center text-primary mb-2">
+        Our All Applications
+      </h1>
+      <p className="text-secondary text-center mb-10">
+        Explore All Apps on the Market developed by us. We code for Millions
+      </p>
+      <div className="flex items-center justify-between py-5 px-10">
+        <div>
+          <h4 className="text-xl font-semibold">({apps.length}) Apps Found</h4>
+        </div>
+        <div className="">
+          <label className="w-87.5 input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input type="search" required placeholder="Search Apps" />
+          </label>
+        </div>
+      </div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10">
+        {apps.map((app) => (
+          <AppCard key={app.id} app={app}></AppCard>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Apps;
--- src/Components/Trending.jsx
+++ src/Components/Trending.jsx
@@ -1,9 +1,9 @@
 import { Download, Star } from "lucide-react";
 import React from "react";
 import AppCard from "./AppCard";
+import { Link } from "react-router";
 
 const Trending = ({ apps }) => {
-  console.log(apps);
   return (
     <div className="my-17.5 md:my-35">
       <h1 className="text-5xl font-semibold text-center text-primary mb-2">
@@ -13,9 +13,14 @@ const Trending = ({ apps }) => {
         Explore All Trending Apps on the Market developed by us
       </p>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 px-10">
-        {apps.map((app) => (
+        {apps.slice(0, 8).map((app) => (
           <AppCard key={app.id} app={app}></AppCard>
         ))}
+      </div>
+      <div className=" mt-12 flex justify-center">
+        <Link className="btn btn-lg" to="/apps">
+          Show All
+        </Link>
       </div>
     </div>
   );
